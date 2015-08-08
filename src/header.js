a6ea5752b1b4693ccc87d8d732876c2647c95e0c
ff5912 <iioka5912@gmail.com>
2015-08-08 22:38:09 +0900

    [add] タイトルリンクにクリックイベント追加: 最新の記事を取得する

--- src/header.js
+++ src/header.js
@@ -3,12 +3,20 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 var Header = React.createClass({
+    propTypes: {
+        wikiList: 'recentchanges'
+    },
+    onClick: function() {
+        this.props.setWiki(this.props.wikiList)
+    },
     render: function() {
         return (
             <nav className="navbar navbar-inverse">
                 <div className="container">
                     <div className="navbar-header">
-                        <Link to="app" className="navbar-brand">MediaWiki Sample</Link>
+                        <Link to="app" className="navbar-brand">
+                            <span onClick={this.onClick}>MediaWiki Sample</span>
+                        </Link>
                     </div>
                 </div>
             </nav>
