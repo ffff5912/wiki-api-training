c679d4b6c2a8812be913af04b12b5504064bf301
ff5912 <iioka5912@gmail.com>
2015-08-09 18:19:51 +0900

    [add] style container

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var WikiList = require('./wiki_list.js');
 var Main = React.createClass({
     render: function() {
         return (
-            <div>
+            <div className="container">
                 <h3>最新の更新</h3>
                 <WikiList wiki={this.props.wiki} />
             </div>
