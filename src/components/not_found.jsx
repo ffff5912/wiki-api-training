ba75b2961ec7f04436b868d6cf4841ea7cc76d3c
ff5912 <iioka5912@gmail.com>
2015-08-20 00:08:37 +0900

    [add] not_found page

---
+++ src/components/not_found.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var NotFound = React.createClass({
+    render: function() {
+        return (
+            <div className="container">
+                <p>Not Found Page</p>
+            </div>
+        );
+    }
+});
+
+module.exports = NotFound;
