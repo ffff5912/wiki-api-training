a07bb1dcf2c856c1f6c26e30f1abaadd1fb5cd94
ff5912 <iioka5912@gmail.com>
2015-08-27 01:13:02 +0900

    カテゴリールートを追加

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,13 @@ var DefaultRoute = Router.DefaultRoute;
 var App = require('./app.jsx');
 var NotFound = require('./components/not_found.jsx');
 var Wiki =require('./components/wiki.jsx');
+var Category =require('./components/category.jsx');
 var Main = require('./components/main.jsx');
 
 var routes = (
     <Route handler={App} name="app" path="/">
         <Route handler={Wiki} name="wiki" path="wiki"/>
+        <Route handler={Category} name="category" path="category"/>
         <DefaultRoute handler={Main}/>
         <NotFoundRoute handler={NotFound}/>
     </Route>
