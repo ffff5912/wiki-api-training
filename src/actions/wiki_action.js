f3aaa5809a64eb6317fe5c37b9083fe2f4974e37
ff5912 <iioka5912@gmail.com>
2015-08-28 00:17:38 +0900

    clean

--- src/actions/wiki_action.js
+++ src/actions/wiki_action.js
@@ -1,5 +1,4 @@
 var ListStorage = require('../stores/list_storage.js');
-var CategoryStorage = require('../stores/category_storage.js');
 
 var WikiAction = {
     get: function(callback, list) {
@@ -10,13 +9,8 @@ var WikiAction = {
     search: function(callback, keyword) {
         ListStorage.findBy(function(res) {
             callback(res);
-        }, keyword)
-    },
-    getCategory: function(callback, keyword) {
-        CategoryStorage.find(function(res) {
-            callback(res);
         }, keyword);
-    },
+    }
 }
 
 module.exports = WikiAction;
