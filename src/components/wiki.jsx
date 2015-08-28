efaa3bfda272287a224ee741dd36991485df91ec
ff5912 <iioka5912@gmail.com>
2015-08-28 23:24:55 +0900

    refactor

--- src/components/wiki.jsx
+++ src/components/wiki.jsx
@@ -1,31 +1,15 @@
 var React = require('react');
 var WikiList = require('./wiki_list.jsx');
+var SearchForm = require('./SearchForm.jsx');
 
-var SearchForm = React.createClass({
-    handleSubmit: function(e) {
-        e.preventDefault();
-        var keyword = React.findDOMNode(this.refs.keyword).value;
-        if (!keyword) {
-            return;
-        }
+var Wiki = React.createClass({
+    handleSubmit: function (keyword) {
         this.props.searchWiki(keyword);
-        return;
     },
     render: function() {
         return (
-                <form onSubmit={this.handleSubmit}>
-                    <input ref="keyword" type="text"/>
-                    <button type="submit" className="btn">検索</button>
-                </form>
-        );
-    }
-});
-
-var Wiki = React.createClass({
-    render: function() {
-        return (
             <div className="container">
-                <SearchForm searchWiki={this.props.searchWiki}/>
+                <SearchForm handleSubmit={this.handleSubmit}/>
                 <WikiList wiki={this.props.wiki}/>
             </div>
         );
